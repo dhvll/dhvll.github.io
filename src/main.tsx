1d1d5bed87b91f8bf72b0ced451e23bbf3749752
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2025-05-08 22:56:23 +0530

    fix css file import for dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import App from "./App.tsx"
-import "./index.css"
 import { RouterProvider } from "@tanstack/react-router"
 import { router } from "./lib/router.tsx"
+import "./styles/globals.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
